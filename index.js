7a380188be6215322231007b7da6e88404f9c714
bittu9835 <bk2671111@gmail.com>
2024-04-18 11:52:04 +0530

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+
+const charset = '@#$%^&*-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*-_?'
+
+const generatePassword = (length) => {
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    return password;
+}
