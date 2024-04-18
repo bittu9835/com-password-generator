1c352a039e5f81cf6ae20dc802eba6ba7a321a94
bittu9835 <bk2671111@gmail.com>
2024-04-18 13:08:02 +0530

    add

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const charset = '@#$%^&*-abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*-_?'
 
-const generatePassword = (length) => {
+export const generatePassword = (length) => {
     let password = '';
     for (let i = 0; i < length; i++) {
         const randomIndex = Math.floor(Math.random() * charset.length);
